36a964c3912d48dc5ee0124e561d77f36caf0b7d
hisamisam <93964536+hisamisam@users.noreply.github.com>
2024-06-24 14:48:41 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ class Graph {
     constructor() {
         this.nodes = [];
         this.edges = [];
-        this.colors = [];
     }
 
     addNode(x, y) {
@@ -42,30 +41,55 @@ class Graph {
 const canvas = document.getElementById('graph-canvas');
 const ctx = canvas.getContext('2d');
 const graph = new Graph();
+let selectedNode = null;
 
 canvas.addEventListener('click', (event) => {
     const rect = canvas.getBoundingClientRect();
     const x = event.clientX - rect.left;
     const y = event.clientY - rect.top;
-    graph.addNode(x, y);
+
+    let clickedNode = null;
+    for (let i = 0; i < graph.nodes.length; i++) {
+        const node = graph.nodes[i];
+        const distance = Math.sqrt((node.x - x) ** 2 + (node.y - y) ** 2);
+        if (distance < 20) {
+            clickedNode = i;
+            break;
+        }
+    }
+
+    if (clickedNode !== null) {
+        if (selectedNode === null) {
+            selectedNode = clickedNode;
+        } else {
+            graph.addEdge(selectedNode, clickedNode);
+            selectedNode = null;
+        }
+    } else {
+        graph.addNode(x, y);
+        selectedNode = null; // reset selection if a new node is added
+    }
+
     drawGraph();
 });
 
 function drawGraph() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    for (let i = 0; i < graph.nodes.length; i++) {
-        ctx.beginPath();
-        ctx.arc(graph.nodes[i].x, graph.nodes[i].y, 20, 0, 2 * Math.PI);
-        ctx.fillStyle = graph.nodes[i].color !== null ? getColor(graph.nodes[i].color) : '#ffffff';
-        ctx.fill();
-        ctx.stroke();
-    }
+    
     for (let i = 0; i < graph.edges.length; i++) {
         const node1 = graph.nodes[graph.edges[i][0]];
         const node2 = graph.nodes[graph.edges[i][1]];
         ctx.beginPath();
         ctx.moveTo(node1.x, node1.y);
         ctx.lineTo(node2.x, node2.y);
+        ctx.stroke();
+    }
+
+    for (let i = 0; i < graph.nodes.length; i++) {
+        ctx.beginPath();
+        ctx.arc(graph.nodes[i].x, graph.nodes[i].y, 20, 0, 2 * Math.PI);
+        ctx.fillStyle = graph.nodes[i].color !== null ? getColor(graph.nodes[i].color) : '#ffffff';
+        ctx.fill();
         ctx.stroke();
     }
 }
